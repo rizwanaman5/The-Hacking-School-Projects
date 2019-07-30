0a72b5fc60bb40877ae2d601dd4f500190625cda
Rizwan Aman <rizwanaman5@gmail.com>
2019-07-30 11:58:43 +0530

    week 4

--- Problem Sets/objectOrientedProg.js
+++ Problem Sets/objectOrientedProg.js
@@ -1,10 +1,5 @@
-const http = require('http');
-const port = 8000;
-
-const server = http.createServer(function(request, response) {
-    response.end('My First server');
-});
-
-server.listen(port, function(){
-    console.log(`Server started at port ${port}`);
-})
+class Students {
+    constructor(fname, lname, age, gender, address) {
+        this.name
+    }
+}
